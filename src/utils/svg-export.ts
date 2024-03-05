cbce8a8fed9ffc6614692a9a99cc0f8f4f5b1a5c
3rd <3rd@users.noreply.github.com>
2024-03-05 23:42:10 +0200

    fix: hack to make svg export work on firefox

--- src/utils/svg-export.ts
+++ src/utils/svg-export.ts
@@ -5,53 +5,64 @@ import reactFlowStyle from "../reactflow.css?inline";
 const CONTAINER_QUERY = ".react-flow__viewport";
 const PADDING = 18;
 
+const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
+
 export const exportReactFlowToSVG = async (width: number, height: number, transform = "none") => {
-  const container = document.querySelector(CONTAINER_QUERY);
-  if (!container) throw new Error(`Could not find container with query: ${CONTAINER_QUERY}`);
+  return new Promise<string>((resolve) => {
+    const container = document.querySelector(CONTAINER_QUERY);
+    if (!container) throw new Error(`Could not find container with query: ${CONTAINER_QUERY}`);
 
-  const iframe = document.createElement("iframe");
-  iframe.style.width = `${width + PADDING * 2}px`;
-  iframe.style.height = `${height + PADDING * 2}px`;
-  iframe.style.position = "absolute";
-  // iframe.style.top = "0";
-  // iframe.style.left = "0";
-  iframe.style.top = "150%";
-  iframe.style.left = "150%";
-  document.body.append(iframe);
+    const iframe = document.createElement("iframe");
+    iframe.style.width = `${width + PADDING * 2}px`;
+    iframe.style.height = `${height + PADDING * 2}px`;
+    iframe.style.position = "absolute";
+    // iframe.style.top = "0";
+    // iframe.style.left = "0";
+    iframe.style.top = "150%";
+    iframe.style.left = "150%";
 
-  const iframeDocument = iframe.contentDocument;
-  if (!iframeDocument) throw new Error("Could not get iframe document");
+    iframe.addEventListener("load", () => {
+      const iframeDocument = iframe.contentDocument;
+      if (!iframeDocument) throw new Error("Could not get iframe document");
 
-  const iframeStyle = iframeDocument.createElement("style");
-  iframeStyle.innerHTML = `
-    ${mainStyle + reactFlowStyle}
-    * {
-      box-sizing: border-box;
-    }
-    svg {
-      overflow: visible;
-    }
-    svg > g {
-      transform: translate(${PADDING}px, ${PADDING}px);
-    }
-    .react-flow__nodes {
-      transform: translate(${PADDING}px, ${PADDING}px);
-    }
-  `;
-  iframeDocument.body.append(iframeStyle);
+      const iframeStyle = iframeDocument.createElement("style");
+      iframeStyle.innerHTML = `
+        ${mainStyle + reactFlowStyle}
+        * {
+          box-sizing: border-box;
+        }
+        svg {
+          overflow: visible;
+        }
+        svg > g {
+          transform: translate(${PADDING}px, ${PADDING}px);
+        }
+        .react-flow__nodes {
+          transform: translate(${PADDING}px, ${PADDING}px);
+        }
+      `;
+      iframeDocument.body.append(iframeStyle);
 
-  const clone = container.cloneNode(true) as HTMLElement;
-  Object.assign(clone.style, {
-    transform,
-    width: `${width}px`,
-    height: `${height}px`,
+      const clone = container.cloneNode(true) as HTMLElement;
+      Object.assign(clone.style, {
+        transform,
+        width: `${width}px`,
+        height: `${height}px`,
+      });
+      iframeDocument.body.append(clone);
+
+      setTimeout(
+        () => {
+          const svgDocument = elementToSVG(iframeDocument.documentElement);
+          const result = new XMLSerializer().serializeToString(svgDocument);
+          resolve(result);
+          iframe.remove();
+        },
+        isFirefox ? 500 : 0
+      );
+    });
+    document.body.append(iframe);
   });
-  iframeDocument.body.append(clone);
-
-  const svgDocument = elementToSVG(iframeDocument.documentElement);
-  iframe.remove();
-
-  return new XMLSerializer().serializeToString(svgDocument);
 };
 
 export const downloadSVG = async (svgString: string, filename: string) => {
