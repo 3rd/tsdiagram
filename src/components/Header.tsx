9511edf543a356ef0a59f0429965c5055874886a
Ben Dewey <ben.dewey@ey.com>
2024-07-31 09:48:40 -0400

    applied pulse branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,19 +63,11 @@ export const Header = memo(({ onPreferencesClick, onShareClick }: HeaderProps) =
           </button>
 
           {/* logo */}
-          <div className="hidden items-center text-lg font-bold leading-none sm:flex">
+          <div className="hidden items-center text-md font-bold leading-none sm:flex">
             <span className="py-1 px-0.5 mr-0.5 rounded" style={{ background: "#3178c6" }}>
-              TS
+              PULSE
             </span>
-            <span>Diagram</span>
-            <iframe
-              className="hidden ml-4 opacity-20 sm:block hover:opacity-100"
-              height="20"
-              sandbox="allow-scripts allow-popups"
-              src="https://ghbtns.com/github-btn.html?user=3rd&repo=tsdiagram&type=star&count=true"
-              title="GitHub"
-              width="100"
-            />
+            <span>Schema&nbsp;Diagram</span>
           </div>
         </div>
 
