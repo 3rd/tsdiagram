ae40cf9d48e0a3d7ddc269a93190672008b2e330
3rd <3rd@users.noreply.github.com>
2024-08-05 00:03:55 +0300

    chore: only enable analytics in development

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,13 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+
+if (process.env.NODE_ENV !== "development") {
+  document.addEventListener("DOMContentLoaded", () => {
+    const umamiScript = document.createElement("script");
+    umamiScript.src = "https://umami.dev.pet/script.js";
+    umamiScript.dataset.websiteId = "4c0a2abe-8e94-4a06-89b4-0915ffd69018";
+    umamiScript.defer = true;
+    document.head.append(umamiScript);
+  });
+}
